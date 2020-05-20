eb18e02de8946f51bbc81a21b3ba84897ba8b7a5
Maciej Fraszczak <218741@student.pwr.wroc.pl>
2020-05-20 21:42:51 +0200

    added images loading

--- project/main.py
+++ project/main.py
@@ -1,9 +1,38 @@
 import numpy as np
 import cv2
-from myplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
-training_images = []    # half of pictures of each object
-testing_images = []     # second half, remaining pictures
-name = 'obj'            # variable responsible for navigating between objects
+training_images = []     # half of pictures of each object
+testing_images = []      # second half, remaining pictures
+nr_array = np.arange(72) # array with numbers for randomizing images in both sets
+obj = 'obj'              # variable responsible for navigation between objects' images
+                         # Path to folder containing objects' images at local repository
+path = 'C:/Users/Veteran/Object-recognition-using-SVM-models/'
 
+
+for i in range(1,11,1):         # loop over particular objects' folders
+    obj_nr = obj + str(i)       # storing object's number (label)
+    np.random.shuffle(nr_array) # randomizing images of particular object
+
+    # Creating two sets of earlier separated images
+    training_images_ids = nr_array[:len(nr_array)//2] 
+    testing_images_ids = nr_array[len(nr_array)//2:] 
+
+    for k in range(36):         # loop over images
+        # Building paths according to earlier established sets
+        objpath_train = path + obj_nr + '/' + obj_nr + '__' + str(training_images_ids[k]) + '.png'
+        objpath_test = path + obj_nr + '/' + obj_nr + '__' + str(testing_images_ids[k]) + '.png'
+
+        # Loading images into separate sets (honouring labels)
+        training_image = cv2.imread(objpath_train)
+        training_images.append([training_image, i])
+        testing_image = cv2.imread(objpath_test)
+        testing_images.append([testing_image, i])
+
+    # Calculating edges for each image
+
+
+    # Calculating Hu's invariant values for each image
+
+    # Combining data with objects' labels (obj_nr)
 
