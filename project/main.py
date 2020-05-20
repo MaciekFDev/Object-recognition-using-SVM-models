807e0dc59f85a4598d5078aeb10f5db9cf9fe5bb
Maciej Fraszczak <218741@student.pwr.wroc.pl>
2020-05-20 18:35:43 +0200

    a

--- project/main.py
+++ project/main.py
@@ -2,3 +2,8 @@ import numpy as np
 import cv2
 from myplotlib import pyplot as plt
 
+training_images = []    # half of pictures of each object
+testing_images = []     # second half, remaining pictures
+name = 'obj'            # variable responsible for navigating between objects
+
+
